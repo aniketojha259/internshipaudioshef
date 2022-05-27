88ad24550a9e8a5dc2929f7ece8dd740094b4c69
Aniket ojha <63443863+aniketojha259@users.noreply.github.com>
2022-05-27 20:08:01 +0530

    Initial commit

---
+++ public/explore.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button, TextInput } from "react-native";
+function explore() {
+  return (
+    <>
+      <textInput />
+      <Button>explore</Button>
+    </>
+  );
+}
+export default explore;
---
+++ public/profile.js
@@ -0,0 +1,5 @@
+import React from "react";
+function profile() {
+  return <Text>This is a profile page</Text>;
+}
+export default profile;
